6f29706f9549d3901da9195715a1566011a41297
tdpetrou <petrou.theodore@gmail.com>
2018-01-29 15:44:56 -0500

    added type hints

--- dexplo/math_columns.py
+++ dexplo/math_columns.py
@@ -1,32 +1,34 @@
+from typing import List
 import numpy as np
+from numpy import ndarray
 
 
-def sum_columns(arrs):
+def sum_columns(arrs: List[ndarray]) -> ndarray:
     arr = arrs[0] + arrs[1]
     for arr_other in arrs[2:]:
         arr += arr_other
     return arr
 
 
-def max_columns(arrs):
+def max_columns(arrs: List[ndarray]):
     return np.max(arrs, 0)
 
 
-def min_columns(arrs):
+def min_columns(arrs: List[ndarray]):
     return np.min(arrs, 0)
 
 
-def mean_columns(arrs):
+def mean_columns(arrs: List[ndarray]):
     return np.mean(arrs, 0)
 
 
-def any_columns(arrs):
+def any_columns(arrs: List[ndarray]):
     return np.any(arrs, 0)
 
 
-def all_columns(arrs):
+def all_columns(arrs: List[ndarray]):
     return np.all(arrs, 0)
 
 
-def count_columns(arrs):
+def count_columns(arrs: List[ndarray]):
     return np.sum(arrs, 0)
